1ff0bfa76ef50a41b2c0ae269cc53ed78c4c909c
Tom B. Parker <tombap8@hotmail.com>
2022-11-07 15:14:33 +0900

    Update 1107.sql

--- 02.SQL-Root/03.TODAY/1107.sql
+++ 02.SQL-Root/03.TODAY/1107.sql
@@ -27,3 +27,32 @@ SELECT `first_name`,`company`,`business_phone` FROM `customers` WHERE `company`=
 
 -- 쿼리5 : `customers`테이블
 SELECT `first_name` AS "이름",`company` AS "회사명",`business_phone` AS "연락처" FROM `customers` WHERE `company`="Company K";
+
+-- 참고2) 만약 두개 이상의 컬럼을 합쳐서 출력할 경우
+--     CONCAT(컬럼명1,컬럼명2,...) 
+--     CONCAT함수를 사용한다!
+
+-- 쿼리6 : `customers`테이블
+SELECT CONCAT(`first_name`," ",`last_name`) AS "이름",`company` AS "회사명",`business_phone` AS "연락처" FROM `customers` WHERE `company`="Company K";
+
+-- <<<< 검색에 사용되는 select >>>>>>>
+-- -> LIKE 키워드! + "%"
+-- -> %를 뒤에 붙여 like검색하면 어떤문자로 시작하는 모든 것 검색
+
+-- 쿼리7 : `customers`테이블
+SELECT CONCAT(`first_name`," ",`last_name`) AS "이름", `company` AS "회사명", `business_phone` AS "연락처" FROM `customers` WHERE `first_name` LIKE "A%";
+
+-- -> %를 앞에 붙여서 like검색하면 어떤문자로 끝나는 모든 것 검색
+-- 쿼리8 : `customers`테이블
+SELECT CONCAT(`first_name`," ",`last_name`) AS "이름", `company` AS "회사명", `business_phone` AS "연락처" FROM `customers` WHERE `first_name` LIKE "%n";
+
+
+-- -> %를 앞뒤에 붙여서 like검색하면 어떤문자가 들어간 모든 것 검색
+-- 쿼리9 : `customers`테이블
+SELECT CONCAT(`first_name`," ",`last_name`) AS "이름", `company` AS "회사명", `business_phone` AS "연락처" FROM `customers` WHERE `first_name` LIKE "%ar%";
+
+-- -> 언더바(_)를 사용할 개수만큼 글자수의 제한을 주어 
+-- 그것이상의 모든 것 검색
+
+-- 쿼리10 : `customers`테이블
+SELECT CONCAT(`first_name`," ",`last_name`) AS "이름", `company` AS "회사명", `business_phone` AS "연락처" FROM `customers` WHERE `first_name` LIKE "a_______%";
